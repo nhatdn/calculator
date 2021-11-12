2ecac9d06f031e9e769a297ab73bcd3e083efb91
nhatdn <nhatdn.it@gmail.com>
2021-11-12 13:04:02 +0700

    update error

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ let showData = document.querySelector(".show-calculate");
 let showResult = document.querySelector(".show-result");
 document.querySelectorAll(".items").forEach(item => {
     item.addEventListener('click',(e) =>{
-        if(e.target.innerHTML == "C") {
+        if(e.target.classList.contains("arrow") && showData.innerHTML.length != 0) {
+            showData.innerHTML = showData.innerHTML.substr(0, showData.innerHTML.length - 1);
+        } else if(e.target.innerHTML == "C") {
             showData.innerHTML = "";
             showResult.innerHTML = "0";
         } else if(e.target.innerHTML == "=") {
             let calculate = showData.innerHTML;
             calculate = calculate.replace("x","*").trim();
             try {
-                let result = eval(calculate);
+                let result = eval(calculate).toString();
+                result = result.toString();
+                if(result == "Infinity" || result == "NaN" || result == "undefined") {
+                    showResult.innerHTML = "Error";
+                } else
                 showResult.innerHTML = result;
             } catch {
                 showResult.innerHTML = "Error";
